3fc89343c20a1dda59bb8e51f1723a1adffb699d
Dikendev <diken.dev@gmail.com>
2024-04-14 22:53:28 -0300

    feat add water amount const / multiplier / message error

---
+++ src/application/recipes/recipe.const.ts
@@ -0,0 +1,4 @@
+export const WATER_AMOUNT_MULTIPLIER = 2.5;
+export const WATER_AMOUNT_MIN_MULTIPLIER = 2;
+export const WATER_MIN_MESSAGE_ERROR =
+  'Do not go below a multiplication factor of 2.0 or you may end up with undissolved pockets of lye in your soap.';
