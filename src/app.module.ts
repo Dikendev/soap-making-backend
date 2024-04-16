a54dd49f315fae35defa738aa6d3dbb173df059f
Dikendev <diken.dev@gmail.com>
2024-04-15 23:28:10 -0300

    feat add app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CheerioModule } from './external/cheerio/cheerio.module';
+import { TranslateModule } from './external/translation/translate.module';
 
 @Module({
-  imports: [],
+  imports: [CheerioModule, TranslateModule],
   controllers: [AppController],
   providers: [AppService],
 })
