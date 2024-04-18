26f5a1996d7dc06c60d17f2c77b87a718294e5fa
Dikendev <diken.dev@gmail.com>
2024-04-17 23:35:31 -0300

    feat add SaponificationTableArray interface

--- src/application/ports/services/interfaces/scrape-data.interface.ts
+++ src/application/ports/services/interfaces/scrape-data.interface.ts
@@ -8,3 +8,20 @@ export interface Content {
   KOH: number;
   INCIName: string;
 }
+
+export interface SaponificationTableArray {
+  SAP: string;
+  NAOH: number;
+  KOH: number;
+  names: Name[];
+  INCIName: INCIName[];
+}
+
+export interface INCIName {
+  language: string;
+  name: string;
+}
+export interface Name {
+  language: string;
+  name: string;
+}
