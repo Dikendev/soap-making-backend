e3ea6269f07c2f9b9bd1b082e54cc360c070b580
Dikendev <diken.dev@gmail.com>
2024-04-20 22:13:55 -0300

    feat update module to needed dependency

--- src/application/infrastructure/oil.module.ts
+++ src/application/infrastructure/oil.module.ts
@@ -3,9 +3,10 @@ import { OilPrismaService } from '../ports/services/oil/oil-prisma.service';
 import { OilRepository } from '../repository/oil-repository';
 import { OilController } from '../ports/controllers/oil.controller';
 import { PrismaModule } from '../../external/prisma/prisma.module';
+import { ScrapeDataModule } from './scrape-data.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ScrapeDataModule],
   providers: [
     OilPrismaService,
     { provide: OilRepository, useClass: OilPrismaService },
