4fb61defc046ddec19a89a7ac3487d2a3c8d8154
Dikendev <diken.dev@gmail.com>
2024-04-20 22:16:25 -0300

    feat add find by name / save / register new language

--- src/application/ports/controllers/oil.controller.ts
+++ src/application/ports/controllers/oil.controller.ts
@@ -1,14 +1,29 @@
-import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
-import { OilRepository } from '../../repository/oil-repository';
+import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
+import {
+  FindOilByNameQuery,
+  OilRepository,
+} from '../../repository/oil-repository';
 import { CreateOilDto, OilResponse, OilsDto } from '../services/oil/model';
+import { ScrapeDataLanguageDto } from './scrape-data.controller';
+import { ScrapeDataRepository } from '../../repository/scrape-data.repository';
 
 @Controller('oil')
 export class OilController {
-  constructor(private readonly oilRepository: OilRepository) {}
+  constructor(
+    private readonly oilRepository: OilRepository,
+    private readonly scrapeRepository: ScrapeDataRepository,
+  ) {}
 
   @Get('find-many')
   async findOils(): Promise<OilResponse[]> {
     return this.oilRepository.findOils();
+  }
+
+  @Get('find-by-name')
+  async findOilByName(
+    @Query() findOilByNameQuery: FindOilByNameQuery,
+  ): Promise<OilResponse> {
+    return this.oilRepository.findOilByName(findOilByNameQuery);
   }
 
   @Post('register')
@@ -16,9 +31,26 @@ export class OilController {
     return this.oilRepository.registerOil(createOilDto);
   }
 
-  @Post('register-many')
-  async registerManyOils(@Body() oilsDto: OilsDto): Promise<string> {
-    return this.oilRepository.registerManyOils(oilsDto);
+  @Post('save')
+  async saveScrapeData(@Body() scrapeDataLanguageDto: ScrapeDataLanguageDto) {
+    const scrapeData = await this.scrapeRepository.translateScrapedData(
+      scrapeDataLanguageDto.targetLanguage,
+    );
+
+    if (Array.isArray(scrapeData)) {
+      return this.oilRepository.registerManyOils(
+        scrapeData as unknown as OilsDto,
+      );
+    }
+  }
+
+  @Post('register-new-language')
+  async registerNewLanguage(
+    @Body() scrapeDataLanguageDto: ScrapeDataLanguageDto,
+  ): Promise<string> {
+    return this.oilRepository.registerNewTranslationToExistingOil(
+      scrapeDataLanguageDto,
+    );
   }
 
   @Delete('delete-all')
