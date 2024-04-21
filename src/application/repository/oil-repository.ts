8aa075b6617b2a3871fec9cdd4c1e02177beb3aa
Dikendev <diken.dev@gmail.com>
2024-04-21 15:24:43 -0300

    feat add find oil by name

--- src/application/repository/oil-repository.ts
+++ src/application/repository/oil-repository.ts
@@ -1,25 +1,32 @@
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
-import { OilResponse, OilsDto } from '../ports/services/oil/model';
+import { IsNotEmpty, IsString, Length, MinLength } from 'class-validator';
+import {
+  OilModelResponse,
+  OilResponse,
+  OilsDto,
+} from '../ports/services/oil/model';
 import { DataToUpdate } from '../ports/services/oil/oil-prisma.service';
 import { ScrapeDataLanguageDto } from '../ports/controllers/scrape-data.controller';
 
-export class FindOilByNameQuery {
+export class FindOilQuery {
   @MinLength(5)
   @IsNotEmpty()
   @IsString()
   name: string;
+
+  @Length(3, 3)
+  @IsNotEmpty()
+  @IsString()
+  language: string;
 }
 
 export abstract class OilRepository {
   abstract registerOil(oil: any): Promise<OilResponse>;
   abstract findOils(): Promise<OilResponse[]>;
-  abstract findOilByName(
-    findOilByNameQuery: FindOilByNameQuery,
-  ): Promise<OilResponse>;
+  abstract findOilByName(findOilQuery: FindOilQuery): Promise<OilModelResponse>;
   abstract registerManyOils(oils: OilsDto): Promise<string>;
   abstract deleteAllOils(): Promise<string>;
   abstract registerNewTranslationByName(
-    findOilByNameQuery: FindOilByNameQuery,
+    name: string,
     dataToUpdate: DataToUpdate,
   ): Promise<OilResponse>;
   abstract registerNewTranslationToExistingOil(
