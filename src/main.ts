f07f1b0b20e4c88c62449d929f033e56e6512aaf
Dikendev <diken.dev@gmail.com>
2024-04-21 23:57:18 -0300

    chore add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 const PORT = process.env.PORT;
 
-async function bootstrap() {
+(async () => {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT);
-
-  console.log(`Application is running on: ${await app.getUrl()}`);
-}
-bootstrap();
+  const logger = new Logger('NestApplication');
+  logger.log(`Application is running on: ${PORT}`);
+})();
